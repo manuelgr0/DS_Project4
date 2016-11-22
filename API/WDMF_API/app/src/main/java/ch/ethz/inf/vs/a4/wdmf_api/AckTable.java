7443ed66489dff4a046940b8b11f1414c2bfa994
Unknown <chaehni@ethz.ch>
2016-11-22 16:29:38 +0100

    added delete method

--- API/WDMF_API/app/src/main/java/ch/ethz/inf/vs/a4/wdmf_api/AckTable.java
+++ API/WDMF_API/app/src/main/java/ch/ethz/inf/vs/a4/wdmf_api/AckTable.java
@@ -6,11 +6,9 @@ import java.util.Hashtable;
 public class AckTable {
 
     private Hashtable<String, Hashtable<String, Integer>> hash = new Hashtable<>();
-    private String owner;
 
     public AckTable(String node) {
         insert(node, node, -1);
-        this.owner = node;
     }
 
 
@@ -18,7 +16,7 @@ public class AckTable {
         //private, empty constructor for cloning
     }
 
-    private void insert(String sender, String receiver, Integer value) {
+    public void insert(String sender, String receiver, Integer value) {
 
         System.out.println();
         Hashtable<String, Integer> h;
@@ -32,11 +30,6 @@ public class AckTable {
         h.put(receiver, value);
     }
 
-    public void update (String receiver, Integer seqNo){
-        if(receiver != owner && hash.containsKey(owner) && hash.get(owner).containsKey(receiver))
-            insert(owner, receiver, seqNo);
-    }
-
     public boolean hasKey(String node) {
         return hash.containsKey(node);
     }
@@ -48,24 +41,25 @@ public class AckTable {
 
     public void merge(AckTable other) {
 
+
         for (String keyS : other.hash.keySet()) {
             //This doesn't have keyS
             if (!this.hash.containsKey(keyS)) {
-                this.hash.put(keyS, (Hashtable<String, Integer>) other.hash.get(keyS).clone());
+                this.hash.put(keyS, other.hash.get(keyS));
 
                 for (String n : this.hash.keySet()) {
 
                     if (!other.hash.containsKey(n)) {
                         System.out.println(keyS + "  " + n);
-                        //this.hash.get("b").put("a", -1);
-                        //this.hash.get("a").put("b", -1);
-                        this.insert(keyS, n, -1); // <-- fucked up line
-                        this.insert(n, keyS, -1);
+                        this.hash.get("b").put("a", -1);
+                        this.hash.get("a").put("b", -1);
+                        //this.insert(keyS, n, -1); // <-- fucked up line
+                        //this.insert(n, keyS, -1);
                     }
                 }
 
 
-            }
+            } /*
 
             //This does have keyS
             if (this.hash.containsKey(keyS)) {
@@ -85,7 +79,7 @@ public class AckTable {
 
                 }
 
-            }
+            } */
 
         }
 
@@ -93,7 +87,7 @@ public class AckTable {
 
     public boolean reachedAll(String sender, Integer seqNo) {
         for (String rec : this.hash.get(sender).keySet()) {
-            if (sender != rec && this.hash.get(sender).get(rec)  < seqNo) {
+            if (this.hash.get(sender).get(rec) < seqNo) {
                 return false;
             }
         }
@@ -104,10 +98,7 @@ public class AckTable {
 
     public AckTable clone() {
         AckTable ret = new AckTable();
-        for(String key : this.hash.keySet()){
-            ret.hash.put(key, (Hashtable<String, Integer>) this.hash.get(key).clone());
-        }
-
+        ret.hash = (Hashtable<String, Hashtable<String, Integer>>) this.hash.clone();
         return ret;
     }
 
