fa1b34666e34387ed3ed92d0c579f59c2c7152f5
Jakob Meier <jakobmeier@bluewin.ch>
2016-12-16 13:42:02 +0100

    Interface of MessageBuffer changed => adjusted test method calls

--- API/WDMF_API/app/src/test/java/ch/ethz/inf/vs/a4/wdmf_api/MessageBufferTest.java
+++ API/WDMF_API/app/src/test/java/ch/ethz/inf/vs/a4/wdmf_api/MessageBufferTest.java
@@ -11,6 +11,7 @@ import java.util.Arrays;
 
 import ch.ethz.inf.vs.a4.wdmf_api.local_data.MessageBuffer;
 import ch.ethz.inf.vs.a4.wdmf_api.network_protocol_data.AckTable;
+import ch.ethz.inf.vs.a4.wdmf_api.network_protocol_data.LCTable;
 
 import static org.junit.Assert.*;
 
@@ -18,22 +19,23 @@ import static org.junit.Assert.*;
 public class MessageBufferTest {
     @Test
     public void buildMessageBuffer(){
+        // Create ACK tables
+        AckTable at2 = new AckTable("Manuel");
+        AckTable at1 = new AckTable("Jakob");
+
         // Create buffer with max size 1000B and the name Jakob for the local node
-        MessageBuffer buf = new MessageBuffer("Jakob", 1000);
+        MessageBuffer buf = new MessageBuffer("Jakob", 1000, at1, new LCTable("Jakob"));
 
         // Add some messages
         buf.addLocalMessage(new byte[]{0,1,2,3,4,5,6,7,8,9}, 0);
         buf.addLocalMessage(new byte[]{10,11,12,13,14,15,16,17,18,19}, 0);
         buf.addLocalMessage(new byte[]{20,21,22,23,24,25,26,27,28,29}, 0);
 
-        // Create ACK tables
-        AckTable at1 = new AckTable("Jakob");
-        AckTable at2 = new AckTable("Manuel");
         at1.merge(at2);
         at2.merge(at1);
 
         // Check content of buffer
-        ArrayList<byte[]> toSend = buf.getMessagesForReceiver("Manuel", at1);
+        ArrayList<byte[]> toSend = buf.getEnumeratedMessagesForReceiver("Manuel");
 
         assertEquals(
                 "All messages returned by buffer",
@@ -61,11 +63,10 @@ public class MessageBufferTest {
 
     @Test
     public void memoryLimitTest(){
-        MessageBuffer buf = new MessageBuffer("Jakob", 100);
-
         // Create ACK tables
         AckTable at1 = new AckTable("Jakob");
         AckTable at2 = new AckTable("Manuel");
+        MessageBuffer buf = new MessageBuffer("Jakob", 100, at1, new LCTable("Jakob"));
         at1.merge(at2);
         at2.merge(at1);
 
@@ -73,12 +74,12 @@ public class MessageBufferTest {
         msg[50] = 6;
 
         buf.addLocalMessage(msg, 0);
-        ArrayList<byte[]> toSend1 = buf.getMessagesForReceiver("Manuel", at1);
+        ArrayList<byte[]> toSend1 = buf.getEnumeratedMessagesForReceiver("Manuel");
 
         msg[50] = 0;
         msg[59] = 77;
         buf.addLocalMessage(msg, 0);
-        ArrayList<byte[]> toSend2 = buf.getMessagesForReceiver("Manuel", at1);
+        ArrayList<byte[]> toSend2 = buf.getEnumeratedMessagesForReceiver("Manuel");
 
         // Check content of buffer
 
